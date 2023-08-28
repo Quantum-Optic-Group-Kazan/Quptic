122e9493d2fb3f8cd2b2ce32a67832d62662fcee
Kirill <uljanofff@yandex.ru>
2023-08-28 15:45:25 +0300

    Aded more comments

--- wlm/WLM_methods.py
+++ wlm/WLM_methods.py
@@ -6,8 +6,7 @@ import wlmData
 import wlmConst
 import time
 import matplotlib.pyplot as plt
-import pyvisa
-import ThorlabsPM100
+
 import math
 def set_wl(amount):
     '''
@@ -52,7 +51,7 @@ def wavelength_regulation(cycle_steps, initial_wave, delta_wave, time_pause_set,
         _________________________________________________________________
         Return:
         _________________________________________________________________
-        A dictionary of tuples that consist of 3 parts.
+        A dictionary of tuples that consist of 4 parts.
         1 - time between set_wl and the moment after last pause including all pauses
         2 - wavelength that was set
         3 - wavelength that was got after set to check whether it was set right
@@ -140,7 +139,7 @@ def wl_stabilisation_after_set(wave, time_pause, precision):
     time2 = time.time()
     return round((time2-time1)*1000,2)
 
-def reference_const_PID_stabilisator(mode: bool, reference_wl, koef, max_PID_val, time_pause, timer, start_PID_point = 1860):
+def reference_const_PID_stabilisator(mode: bool, reference_wl, koef, max_PID_val, time_pause, timer, start_PID_point = 4096/2):
     '''
         The function stabilises the reference value of frequency for "timer" seconds;
         2nd version of algorithm
@@ -210,17 +209,20 @@ def reference_const_PID_stabilisator(mode: bool, reference_wl, koef, max_PID_val
             print("Time limit exceeded")
             break
 
+
+# obsolete version of stabiliser
 def wl_stabilisation_through_PID_const(reference_wl, koef, max_dev, time_pause, timer, start_PID_point = 1860):
     '''
-    Function stabilises the wavelength on reference_val
-    1st version of algorithm
-    :param reference_wl: reference value in nm
-    :param koef: koefficient of bond between PID in mV and wavelength as frequency
-    :param max_dev: max deviation in mV
-    :param time_pause: pause after setting PID inside alg-m
-    :param timer: lifetime of function in sec
-    :param start_PID_point: start point for al-m to change PID
-    :return:
+        Function stabilises the wavelength on reference_val
+        1st version of algorithm
+
+        :param reference_wl: reference value in nm
+        :param koef: koefficient of bond between PID in mV and wavelength as frequency
+        :param max_dev: max deviation in mV
+        :param time_pause: pause after setting PID inside alg-m
+        :param timer: lifetime of function in sec
+        :param start_PID_point: start point for al-m to change PID
+        :return:
     '''
     flag = False
     flag2 = True # regulates PID_STEP if laser swims out of reference
@@ -273,16 +275,18 @@ def wl_stabilisation_through_PID_const(reference_wl, koef, max_dev, time_pause, 
         if((time2-time1)>timer):
             break
 
+
+# to tell the truth the find_k_i were not very useful. Finally we got the value of k and just use it. It's up tp the resource laser / WLM
 def find_k(points, PID_step, PID_start, time_pause):
     '''
-      wave - before set
-      wave2 - after set
-      :param points:
-      :param PID_step:
-      :param PID_start:
-      :param time_pause:
-      :param delta_time: wait time until we get another wavelength is our step time_pause was false
-      :return:
+      Finds the coefficient in dependency PID / frequency
+      Uses the average in set of delta_frequency_k/delta_PID_k
+
+      :param points: number of steps
+      :param PID_step: the value of PID step in mV
+      :param PID_start: start PID value
+      :param time_pause: time to pause after setting PID
+      :return: the coefficient in dependency PID / frequency
       '''
     assert points > 2, "Error: 2 or more points needed"
     i = 0
@@ -307,14 +311,15 @@ def find_k(points, PID_step, PID_start, time_pause):
 
 def find_k2(points, PID_step, PID_start):
     '''
-      wave - before set
-      wave2 - after set
-      :param points:
-      :param PID_step:
-      :param PID_start:
-      :param time_pause:
-      :param delta_time: wait time until we get another wavelength is our step time_pause was false
-      :return:
+      Finds the coefficient in dependency PID / frequency
+      Uses the average in set of delta_max-k/delta_PID_max-k, k < points/2
+      In other words we take simply the last and the first item and find the coeff than prelast and first item etc.
+
+      :param points: number of steps
+      :param PID_step: the value of PID step in mV
+      :param PID_start: start PID value
+      :param time_pause: time to pause after setting PID
+      :return: the coefficient in dependency PID / frequency
       '''
     assert points > 2, "Error: 2 or more points needed"
     assert (points%2) == 0, "Error: even points number needed"
@@ -336,25 +341,6 @@ def find_k2(points, PID_step, PID_start):
         k+=1
     return np.average(koefs_list)
 
-def plot_wavelength_PID_find_k(points, PID_step):
-    '''
-    To use the function you need to return dictionary d in find_k function
-    It will show you the graph of dependency between PID in mV and frequency measured in THz
-    :param points: number of different measurements
-    :param PID_step: step of PID in mV to use in find_k / or find_k2.
-    :param time_pause: time to pause after setting the current PID in mV
-    Remark: highly recommended to set delay more than exposition time to get an adequate result
-    :return:
-    '''
-    start_pid = wlmData.dll.GetDeviationSignalNum(1,0)
-    d = find_k2(points, PID_step, start_pid).copy()
-    x = [round(d[i][0],2) for i in range(points)]
-    y = [round(d[i][1],7) for i in range(points)]
-    fig, ax = plt.subplots(figsize=(5,3), layout='constrained')
-    ax.scatter(x,y)
-    plt.ylim(min(y), max(y))
-    plt.show()
-
 def time_counter(ref_frequency):
     '''
     Counts time until we get new measure
@@ -373,6 +359,17 @@ def time_counter(ref_frequency):
 
 def triangle_PID_course(mode: bool, down_reference, upper_reference, stabilisation_time,
                         PID_step_mV, time_limit):
+    '''
+        The function makes triangle PID stepping
+
+        :param mode: Set true if you pass the references in [THz]. Set false if in [nm]
+        :param down_reference: the bottom frequency
+        :param upper_reference: the upper values of frequency
+        :param stabilisation_time: time to stabilise the bottom value
+        :param PID_step_mV: the step of PID in [mv]
+        :param time_limit: limit to perform the al-m
+        :return:
+    '''
     assert stabilisation_time < time_limit, "Error: too short time limit"
     koef = -2.23e-06*1.5225
     d_reference = down_reference
@@ -406,104 +403,4 @@ def triangle_PID_course(mode: bool, down_reference, upper_reference, stabilisati
             break
 
 
-def initialise_power_meter(connection_row):
-    rm = pyvisa.ResourceManager()
-    inst = rm.open_resource(connection_row)
-    power_meter = ThorlabsPM100.ThorlabsPM100(inst=inst)
-    return power_meter
 
-def find_max_mod(absc_list, ord_list):
-    max_mod = max(ord_list)
-    freq_of_max_mod = 0.
-    index = -1
-    for k in range(len(absc_list)):
-        if (ord_list[k] == max_mod):
-            freq_of_max_mod = absc_list[k]
-            index = k
-            break
-    return max_mod,freq_of_max_mod,index
-
-def find_breadth_mod(absc_list, ord_list, index_mod):
-    size = len(absc_list)
-    assert index_mod >= 0 and index_mod < size, "Error: index of mod is out of bounds. Check lists of values and index"
-    k1 = index_mod
-    k2 = index_mod
-    barier = ord_list[index_mod] / 2
-    while(ord_list[k1] >= barier):
-        k1+=1
-    while(ord_list[k2] >= barier):
-        k2-=1
-    assert k1 >= 0 and k2 < size, "Error: index is out of bounds. The mod can be cut off"
-    return absc_list[k1]-absc_list[k2]
-def del_mod(absc_list, ord_list, index_mod, breadth):
-    size = len(absc_list)
-    assert index_mod >= 0 and index_mod < size, "Error: index of mod is out of bounds. Check lists of values and index"
-    k1 = index_mod
-    k2 = index_mod
-    left = absc_list[index_mod] - breadth/2
-    right = absc_list[index_mod] + breadth/2
-    while (absc_list[k1] <= right):
-        k1+=1
-    while (absc_list[k2] >= left):
-        k2 -= 1
-    for _ in range(k1 - k2 + 1):
-        del absc_list[k2]
-        del ord_list[k2]
-def stepping_PID_course(mode: bool, down_reference, upper_reference, stabilisation_time,
-                        PID_step_mV, time_limit):
-    assert stabilisation_time < time_limit, "Error: too short time limit"
-    koef = -2.23e-06 * 1.5225
-    flag = False
-    d_reference = down_reference
-    u_reference = upper_reference
-    delta_freq = koef * PID_step_mV
-    d = {}
-    i = 0
-    if (not mode):
-        d_reference = wlmData.dll.ConvertUnit(d_reference, wlmConst.cReturnWavelengthVac,
-                                              wlmConst.cReturnFrequency)
-        u_reference = wlmData.dll.ConvertUnit(upper_reference, wlmConst.cReturnWavelengthVac,
-                                              wlmConst.cReturnFrequency)
-    max_PID_val = 4096
-
-    power_meter = initialise_power_meter("USB0::0x1313::0x8078::P0008894::INSTR")
-
-    time1 = time.time()
-    start_PID_point = wlmData.dll.GetDeviationSignalNum(1, 0)
-    reference_const_PID_stabilisator(True, d_reference, koef, max_PID_val,
-                                     wlmData.dll.GetExposureNum(1, 1, 0) * 1.2, stabilisation_time, start_PID_point)
-    PID_current = wlmData.dll.GetDeviationSignalNum(1, 0)
-
-    d[i] = (abs(wlmData.dll.ConvertUnit(wlmData.dll.GetWavelengthNum(1, 0), wlmConst.cReturnWavelengthVac,
-                                        wlmConst.cReturnFrequency) - d_reference), power_meter.read)
-    i += 1
-    while (True):
-        flag = False
-        PID_current = PID_current + PID_step_mV
-        cur_freq = wlmData.dll.ConvertUnit(wlmData.dll.GetWavelengthNum(1, 0), wlmConst.cReturnWavelengthVac,
-                                           wlmConst.cReturnFrequency)
-        if (cur_freq + delta_freq > u_reference):
-            percent = (u_reference-cur_freq) / delta_freq
-            if( percent > 0.1 and percent <= 1. and abs(PID_step_mV*percent) >= 0.125):
-                PID_current = PID_current - PID_step_mV
-                PID_current = PID_current + PID_step_mV*percent
-                wlmData.dll.SetDeviationSignalNum(1, PID_current)
-                freq = time_counter(cur_freq)
-                d[i] = (abs(freq - d_reference), power_meter.read)
-                if (d[i][0] / d[i - 1][0] > 1000):
-                    del d[i]
-                    flag = True
-                if (not flag):
-                    i += 1
-            break
-        wlmData.dll.SetDeviationSignalNum(1, PID_current)
-        freq = time_counter(cur_freq)
-        d[i] = (abs(freq - d_reference), power_meter.read)
-        if(d[i][0]/d[i-1][0] > 1000):
-            del d[i]
-            flag = True
-        if(not flag):
-            i += 1
-        if (time.time() - time1 > time_limit):
-            break
-    return d, i
